2cc0febad1d34499cd20a3dda9bd00fc2014f1c7
Elara Services <43923163+Elara-Discord-Bots@users.noreply.github.com>
2024-08-19 06:46:45 -0700

    remove event from cache if it's been completed

--- src/client/actions/GuildScheduledEventUpdate.js
+++ src/client/actions/GuildScheduledEventUpdate.js
@@ -12,14 +12,23 @@ class GuildScheduledEventUpdateAction extends Action {
       const oldGuildScheduledEvent = guild.scheduledEvents.resolve(data.id)?._clone() ?? null;
       const newGuildScheduledEvent = guild.scheduledEvents._add(data);
 
+      if (!oldGuildScheduledEvent || !newGuildScheduledEvent) {
+        return {};
+      }
       /**
        * Emitted whenever a guild scheduled event gets updated.
        * @event Client#guildScheduledEventUpdate
-       * @param {?GuildScheduledEvent} oldGuildScheduledEvent The guild scheduled event object before the update
+       * @param {GuildScheduledEvent} oldGuildScheduledEvent The guild scheduled event object before the update
        * @param {GuildScheduledEvent} newGuildScheduledEvent The guild scheduled event object after the update
        */
       client.emit(Events.GUILD_SCHEDULED_EVENT_UPDATE, oldGuildScheduledEvent, newGuildScheduledEvent);
-
+      if ('status' in newGuildScheduledEvent && newGuildScheduledEvent.status?.toLowerCase?.() === 'completed') {
+        /**
+         * If the new status is 'completed' remove it from the cache after emitting the event.
+         * Otherwise the event is cached until the process gets restarted.
+         */
+        guild.scheduledEvents.cache.delete(data.id);
+      }
       return { oldGuildScheduledEvent, newGuildScheduledEvent };
     }
 
